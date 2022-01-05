2847340d3c15fc434240a9f63e7db3acd4798c34
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-01-04 17:40:37 -0800

    wrote second passing snapshot

---
+++ src/views/Dogs/Dogs.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Dogs from './Dogs';
+
+test('should render a list of dogs', async () => {
+  const { container } = render(
+    <MemoryRouter>
+      <Dogs />
+    </MemoryRouter>
+  );
+  await screen.findByText('Meet Barton');
+  expect(container).toMatchSnapshot();
+});
